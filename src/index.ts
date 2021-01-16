409ed850c08b2a37908af8dbb547b8547257ae47
Dima Iv <ivdim4ik95@gmail.com>
2021-01-16 15:47:34 +0300

    Add description for options of cli

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ import generateComponent from "./generateComponent";
 export = <T extends string[]>(argv: T): void => {
   program
     .command("component [component...]")
-    .option("-p, --path <path>", "", config.path)
-    .option("--withStyle", "", config.withStyle)
-    .option("--usePreprocessor <preprocessor>", "", config.usePreprocessor)
-    .option("--useTypescript", "", config.useTypescript)
+    .option("-p, --path <path>", "The path where you want the component to be generated.", config.path)
+    .option("--withStyle", "Creates a stylesheet file with this component.", config.withStyle)
+    .option("--usePreprocessor <preprocessor>", "Creates a stylesheet file used a preprocessor such as scss with this component.", config.usePreprocessor)
+    .option("--useTypescript", "Creates the typescript component.", config.useTypescript)
     .action((components: string[], args: Config) => {
       components.forEach(c => generateComponent<Config>(c, args));
     });
