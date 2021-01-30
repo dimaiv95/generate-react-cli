d78a810b82b13802521b1967fb09b84c33aadeab
Dima Iv <ivdim4ik95@gmail.com>
2021-01-30 19:14:10 +0300

    Change base template component

--- src/templates/componetTsFunction.ts
+++ src/templates/componetTsFunction.ts
@@ -1,7 +1,7 @@
 const ComponetTsFunction = `import React from "react";
 [importStyle]
 const [TemplateName]: React.FC = () => (
-  <div[className]></div>
+  <div[className]>Hi, I\'m a [TemplateName]</div>
 );
 
 export default [TemplateName];
