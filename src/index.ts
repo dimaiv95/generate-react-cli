980d4bc1f1567ff6806a8bd24782100be93b4956
Dima Iv <ivdim4ik95@gmail.com>
2021-01-30 18:54:54 +0300

    Fix type param in functions generateComponentTemplate and generateComponent

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export = <T extends string[]>(argv: T): void => {
     .option("--useTypescript", "Creates the typescript component.", config.useTypescript)
     .option("--useTest <test>", "Creates test file with this component.", config.useTest)
     .action((components: string[], args: Config) => {
-      components.forEach(c => generateComponent<Config>(c, args));
+      components.forEach(c => generateComponent(c, args));
     });
 
     program.parse(argv);
